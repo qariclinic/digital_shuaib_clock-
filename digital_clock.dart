b6a6bc0cbd86abaf77f3bd0240ac60e2f628bddb
qariclinic <hafizabunouman@gmail.com>
2025-11-25 13:23:53 +0500

    Create digital_clock.dart

---
+++ digital_clock.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:hijri/hijri_calendar.dart';
+
+class DigitalClock extends StatefulWidget {
+  @override
+  _DigitalClockState createState() => _DigitalClockState();
+}
+
+class _DigitalClockState extends State<DigitalClock> {
+  @override
+  Widget build(BuildContext context) {
+    String time = DateFormat('hh:mm:ss a').format(DateTime.now());
+    HijriCalendar.setLocal("ar");
+    String hijri = HijriCalendar.now().toFormat("dd MMMM yyyy");
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(time, style: TextStyle(fontSize: 70, color: Colors.green, fontFamily: 'Digital')),
+            SizedBox(height: 10),
+            Text(hijri, style: TextStyle(fontSize: 30, color: Colors.white)),
+          ],
+        ),
+      ),
+    );
+  }
+}
